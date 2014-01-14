c40bf9d8fa18bab1f80d1b0a6c376c6d24e030a5
jianjundeng <djjsindy@163.com>
2014-01-14 23:46:56 +0800

    build

---
+++ src/queue.c
@@ -0,0 +1,52 @@
+#include "queue.h"
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+queue_t *init_queue(){
+  queue_t *q=(queue_t *)malloc(sizeof(queue_t));
+  pthread_mutex_t mutex;
+  pthread_mutex_init(&mutex, NULL);
+  q->mutex=&mutex;
+  return q;
+}
+
+void detroy_queue(queue_t *t){
+}
+
+void push(queue_t *q,void *data){
+  pthread_mutex_lock(q->mutex);
+  queue_item_t *item=(queue_item_t *)malloc(sizeof(queue_item_t));
+  item->data=data;
+  if(q->tail==NULL&&q->head==NULL){
+    q->tail=item;
+    q->head=item;
+  }else if(q->tail==q->head){  
+    q->head->next=item;
+    q->tail=item;
+  }else{
+    queue_item_t *tail=q->tail;
+    tail->next=item;
+    q->tail=item;
+  }
+  pthread_mutex_unlock(q->mutex);
+}
+
+void* peek(queue_t *q){
+  pthread_mutex_lock(q->mutex);
+  if(q->head==NULL){
+    return 0;
+  }
+  void *data=q->head->data;
+  queue_item_t *t=q->head;
+  if(q->head==q->tail){
+    q->head=NULL;
+    q->tail=NULL;
+  }else{
+    q->head=q->head->next;
+  }
+  free(t);
+  pthread_mutex_unlock(q->mutex);
+  return data;
+}
+
+
---
+++ src/queue.h
@@ -0,0 +1,20 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+struct queue_item_s{
+  void *data;
+  struct queue_item_s *next;
+};
+
+typedef struct queue_item_s queue_item_t;
+
+struct queue_s{
+  queue_item_t *head;
+  queue_item_t *tail;
+  pthread_mutex_t *mutex;
+};
+
+typedef struct queue_s queue_t;
+#endif
